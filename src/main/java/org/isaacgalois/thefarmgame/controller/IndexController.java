6d99bc1bac6cc55c92b53df9b0c2c2a5479d5fa4
Alex Sperry <speral01@luther.edu>
2018-12-01 21:52:29 -0600

    Initial commit

---
+++ src/main/java/org/isaacgalois/thefarmgame/controller/IndexController.java
@@ -0,0 +1,18 @@
+package org.isaacgalois.thefarmgame.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,12 @@
+<%@include file="includes/header.jsp" %>
+<%@include file="includes/navbar.jsp" %>
+<div class="container">
+    <div class="jumbotron">
+        <h2>Welcome to the Farm Game</h2>
+        <p>a game about running a farm.</p>
+    </div>
+</div>
+
+
+
+<%@include file="includes/footer.jsp" %>
